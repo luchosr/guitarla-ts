8cc0b2b7aef09a42d884d862e038f5bb38eda419
Luciano Ramello <luchosr@gmail.com>
2024-08-22 17:29:24 +0200

    feat: adds new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,19 +1,11 @@
 export type Guitar = {
-    id: number
-    name: string
-    image: string
-    description: string
-    price: number
-}
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+};
 
 export type CartItem = Guitar & {
-    quantity: number
-}
-
-// export type CartItem = Pick<Guitar, 'id' | 'name' | 'price' > & {
-//     quantity: number
-// }
-// export type CartItem = Omit<Guitar, 'id' | 'name' | 'price' > & {
-//     quantity: number
-// }
-
+  quantity: number;
+};
