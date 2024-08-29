6c33e0e9712702e87f2e831274e57c5256ca54ee
Luciano Ramello <luchosr@gmail.com>
2024-08-29 20:03:05 +0200

    feat: implements cart actions

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -13,9 +13,14 @@ export type CartState = {
   cart: CartItem[];
 };
 
+const initialCart = (): CartItem[] => {
+  const localStorageCart = localStorage.getItem('cart');
+  return localStorageCart ? [...JSON.parse(localStorageCart)] : [];
+};
+
 export const initialState: CartState = {
   data: db,
-  cart: [],
+  cart: initialCart(),
 };
 
 const MAX_ITEMS = 5;
@@ -33,6 +38,17 @@ export const cartReducer = (
     let updatedCart: CartItem[] = [];
 
     if (itemExists) {
+      updatedCart = state.cart.map((item) => {
+        if (item.id === action.payload.item.id) {
+          if (item.quantity < MAX_ITEMS) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        } else {
+          return item;
+        }
+      });
     } else {
       const newItem: CartItem = { ...action.payload.item, quantity: 1 };
       updatedCart = [...state.cart, newItem];
@@ -43,18 +59,41 @@ export const cartReducer = (
     };
   }
   if (action.type === 'remove-from-cart') {
+    const cart = state.cart.filter((item) => item.id !== action.payload.id);
     return {
       ...state,
+      cart,
     };
   }
   if (action.type === 'decrease-quantity') {
+    const cart = state.cart.map((item) => {
+      if (item.id === action.payload.id && item.quantity > MIN_ITEMS) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
     return {
       ...state,
+      cart,
     };
   }
   if (action.type === 'increase-quantity') {
+    const cart = state.cart.map((item) => {
+      if (item.id === action.payload.id && item.quantity < MAX_ITEMS) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+
     return {
       ...state,
+      cart,
+    };
+  }
+  if (action.type === 'clear-cart') {
+    return {
+      ...state,
+      cart: [],
     };
   }
 
